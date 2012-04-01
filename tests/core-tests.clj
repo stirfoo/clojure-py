bfda6367f54af320a7eec0e84e6ae7ef746a2b8e
stirfoo <stirfoo@gmail.com>
2012-03-31 20:02:24 -0700

    Revert "undo my previous commit" 1 of 5
    This reverts commit cc1882c2c6214bcc646fe2f9d4c1021ab32bb133.

--- tests/core-tests.clj
+++ tests/core-tests.clj
@@ -347,7 +347,10 @@
     (assertions/assert-true (= #{} (set [])))
     (assertions/assert-true (= #{"foo"} (set ["foo"])))
     (assertions/assert-true (= #{1 2 3} #{1 3 2}))
-; FIXME vector/map find (assertions/assert-true (= #{#{1 2 3} [4 5 6] {7 8} 9 10} #{10 9 [4 5 6] {7 8} #{1 2 3}}))
+    (assertions/assert-true (= #{#{1 2 3} [4 5 6] {7 8} 9 10}
+                               #{10 9 [4 5 6] {7 8} #{1 2 3}}))
+    (assertions/assert-false (= #{10 9 [4 5 6] {7 8} #{1 2 3} "foo"}
+                               #{#{1 2 3} [4 5 6] {7 8} 9 10}))
     (assertions/assert-true (= #{#{1 2 3} 9 10} #{10 9 #{1 2 3}}))
     (assertions/assert-true (not (= #{nil [] {} 0 #{}} #{})))
     (assertions/assert-true (= (count #{nil [] {} 0 #{}}) 5))
@@ -358,9 +361,10 @@
     (assertions/assert-true (contains? #{1 2 3 4} 4))
     (assertions/assert-true (contains? #{[] nil 0 {} #{}} {}))
     (assertions/assert-true (contains? #{[1 2 3]} [1 2 3]))
-    ;; FIXME
-    ;; (assertions/assert-false (= [] {}))
+    (assertions/assert-false (= [] {}))
+    (assertions/assert-false (= {} []))
     (assertions/assert-false (= () #{}))
+    (assertions/assert-false (= #{} ()))
     (assertions/assert-true (= () []))
     (assertions/assert-true (= [] ()))
     (assertions/assert-true (= #{1 2 3} #{1 2 3}))
